631e477b54cd959025118512c6419158b8c3fed2
Roelof Roos <github@roelof.io>
2017-03-15 13:07:46 +0100

    Added example code (broken)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    var sayHello = function() {
+        console.log('Mein little Füher');
+    };
+
+    document.addEventListener('DOMContentLoaded', sayHello);
+});
